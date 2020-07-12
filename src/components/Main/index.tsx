51bdcf1bb54233ad292c9955f7f57a5583db4681
Henrique Almeida <henrique_af1@msn.com>
2020-07-12 11:52:57 -0300

    Create Header and BottomMenu

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon,
+} from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Guilherme Rodz</strong>
+          <span>612 tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/* <ProfilePage /> */}
+
+      <BottomMenu>
+        <HomeIcon className="active" />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu>
+    </Container>
+  );
+};
+
+export default Main;
